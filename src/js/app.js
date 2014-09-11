d70caf215d81a6266e1a63669e440c41ee28ece5
onaliugo <contact@onaliugo.fr>
2014-09-11 11:04:29 +0200

    Add some useful JS libs to drop jQuery/Zepto

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,31 @@
-var str = require('./tools/str');
-console.log(str);
+var domready = require('domready');
+var qwery = require('qwery');
+var bonzo = require('bonzo');
+var bean = require('bean');
+var localLogger = require('./logger.js');
+
+domready(function() {
+  console.log('domready!');
+
+  localLogger('pouet');
+
+  // query selector engine
+  // https://github.com/ded/qwery
+  var $p = qwery('p');
+
+  // extensible DOM utility
+  // https://github.com/ded/bonzo
+  bonzo($p)
+    .addClass('pouet')
+    .css({
+      color: '#444',
+      marginLeft: '10px'
+    });
+
+  // events api for javascript
+  // https://github.com/fat/bean
+  bean.on(document.body, 'click', function(e){
+    console.log('body clicked', e);
+  });
+
+});
